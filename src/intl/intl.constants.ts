2ea1758d16d49237949fa72a4009cd5c5af8a2c1
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-08-09 18:01:54 -0300

    Add supported Locales to crowdin languages code map

--- src/intl/intl.constants.ts
+++ src/intl/intl.constants.ts
@@ -3,3 +3,12 @@ export const supportedLocales = ['en-US', 'pt-BR', 'es-ES', 'fr'];
 export const defaultLocale = 'en-US';
 
 export const localePrefix = 'always';
+
+//See https://developer.crowdin.com/language-codes/
+export const mapSupportedLocalesToCrowdinLanguageCodes: Record<string, string> =
+  {
+    'en-US': 'en',
+    'pt-BR': 'pt-BR',
+    'es-ES': 'es-ES',
+    'fr-FR': 'fr',
+  };
--- src/intl/scripts/getSupportedLocales.ts
+++ src/intl/scripts/getSupportedLocales.ts
@@ -1,5 +1,19 @@
-import { supportedLocales, defaultLocale } from '../intl.constants';
+import {
+  supportedLocales,
+  mapSupportedLocalesToCrowdinLanguageCodes,
+} from '../intl.constants';
+
 const result = supportedLocales
-  .map((locale) => (locale === defaultLocale ? '' : `-l ${locale}`))
+  .map((locale) => {
+    if (!(locale in mapSupportedLocalesToCrowdinLanguageCodes)) {
+      throw new Error(
+        `The locale "${locale}" is not supported by Crowdin. Please add it to the "mapSupportedLocalesToCrowdinLanguageCodes" object in "src/intl/intl.constants.ts".`,
+      );
+    }
+
+    const crowdinLanguageCode =
+      mapSupportedLocalesToCrowdinLanguageCodes[locale];
+    return `-l ${crowdinLanguageCode}`;
+  })
   .join(' ');
 console.log(result);
