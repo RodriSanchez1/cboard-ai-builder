3ce333d18ffa3ce309f8303345cc31c5bde3adb3
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-08-09 18:08:01 -0300

    wip

--- src/intl/intl.constants.ts
+++ src/intl/intl.constants.ts
@@ -7,7 +7,6 @@ export const localePrefix = 'always';
 //See https://developer.crowdin.com/language-codes/
 export const mapSupportedLocalesToCrowdinLanguageCodes: Record<string, string> =
   {
-    'en-US': 'en',
     'pt-BR': 'pt-BR',
     'es-ES': 'es-ES',
     'fr-FR': 'fr',
